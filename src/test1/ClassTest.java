79082d88bd825078c73117cf018fffc4ffd0c672
Doryan <Doryan@DESKTOP-CR7V76U>
2022-02-08 08:39:45 +0100

    modification message console.

--- src/test1/ClassTest.java
+++ src/test1/ClassTest.java
@@ -3,7 +3,7 @@ package test1;
 public class ClassTest {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello world !");
 		// TODO Auto-generated method stub
 
 	}
